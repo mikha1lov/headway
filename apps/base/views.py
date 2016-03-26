4648478515d1da5eb4ddca3658fef46d624a8126
shapoglyk <abylov.belek@gmail.com>
2016-03-27 03:30:15 +0600

    Events calendar on Home page.

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,6 +1,8 @@
+import json
 from django.shortcuts import render
 from django.utils import timezone
 
+from django.core import serializers
 from apps.base.models import News
 from apps.event.models import Event
 
@@ -9,4 +11,7 @@ def home(request):
     slider_events = Event.objects.filter(data__lt=timezone.now())
     events = Event.objects.filter(data__gte=timezone.now())
     news = News.objects.all()[:10]
+    clndr_events = serializers.serialize("json", events, indent=2, use_natural_foreign_keys=True,
+                                         use_natural_primary_keys=True, fields=('title', 'data'))
+    print(clndr_events)
     return render(request, 'base/home.html', locals())
