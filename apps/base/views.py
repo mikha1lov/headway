d153135ddc359b35066fba9e80daea46b9f0b6fa
shapoglyk <abylov.belek@gmail.com>
2016-03-26 17:27:58 +0600

    Display events and news on home tpl

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 
+from apps.base.models import News
 from apps.event.models import Event
 
 
 def home(request):
     slider_events = Event.objects.filter(data__lt=timezone.now())
     events = Event.objects.filter(data__gte=timezone.now())
-
+    news = News.objects.all()[:10]
     return render(request, 'base/home.html', locals())
