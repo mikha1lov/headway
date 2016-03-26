9a190e2067df6c3ea13f76354046f7fc820039c9
mirotvorez <volkik91@gmail.com>
2016-03-26 19:45:46 +0600

    Header is done

---
+++ static/js/scripts.js
@@ -0,0 +1,13 @@
+/**
+ * Created by m1rotvorez on 26.03.16.
+ */
+$(document).ready(function(){
+  $('.main_slider').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true
+
+  });
+
+});
