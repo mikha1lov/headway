9b02688f855a1fb1971cdf15308367a9f34448d6
shapoglyk <abylov.belek@gmail.com>
2016-03-26 17:24:50 +0600

    Added News model. Modified home view.

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,5 +1,21 @@
+# coding: utf-8
 from __future__ import unicode_literals
 
 from django.db import models
+from redactor.fields import RedactorField
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(u"Заголовок", max_length=255)
+    photo = models.ImageField(u"Фотография", upload_to='news/')
+    text = RedactorField(verbose_name=u"Описание",
+                         redactor_options={
+                             'lang': 'en',
+                         })
+
+    class Meta:
+        verbose_name = u"Новость"
+        verbose_name_plural = u"Новость"
+
+    def __unicode__(self):
+        return self.title
--- apps/base/views.py
+++ apps/base/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from apps.event.models import Event
 
 
 def home(request):
+    slider_events = Event.objects.filter(data__lt=timezone.now())
+    events = Event.objects.filter(data__gte=timezone.now())
 
     return render(request, 'base/home.html', locals())
