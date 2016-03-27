7a7a672ecb61b4a242144831584ca6bab2e55e3e
mirotvorez <volkik91@gmail.com>
2016-03-27 16:22:19 +0600

    Libraries form and table

--- apps/library/forms.py
+++ apps/library/forms.py
@@ -8,3 +8,8 @@ class BookOrderForm(forms.ModelForm):
     class Meta:
         model = BookOrder
         exclude = ('item', )
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form_name', 'placeholder':u'ФИО'}),
+            'contacts': forms.TextInput(attrs={'class':'form_contacts', 'placeholder':u'E-mail или телефон'}),
+            'note': forms.Textarea(attrs={'class':'form_note', 'placeholder':u'Комментарий'}),
+        }
